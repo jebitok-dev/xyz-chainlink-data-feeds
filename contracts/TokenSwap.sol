bc642da45b80f6d70d5a9ebae57b5c7ff9233f3e
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-03-26 18:45:20 +0300

    Add Changes to Contract

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -3,19 +3,6 @@ pragma solidity ^0.8.10;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-/*
-How to swap tokens
-
-1. Alice has 100 tokens from AliceCoin, which is a ERC20 token.
-2. Bob has 100 tokens from BobCoin, which is also a ERC20 token.
-3. Alice and Bob wants to trade 10 AliceCoin for 20 BobCoin.
-4. Alice or Bob deploys TokenSwap
-5. Alice approves TokenSwap to withdraw 10 tokens from AliceCoin
-6. Bob approves TokenSwap to withdraw 20 tokens from BobCoin
-7. Alice or Bob calls TokenSwap.swap()
-8. Alice and Bob traded tokens successfully.
-*/
-
 contract TokenSwap {
     AggregatorV3Interface internal priceFeed;
 
